918682e3d10ccaa0ac4c2ce2f683b3acc3fba943
miocoder <mjovanov@outlook.com>
2016-12-26 19:36:34 +0100

    Implemented Module1, very minor manual testing done.

---
+++ module1-solution/public_html/js/app.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', MsgController);
+
+MsgController.$inject = ['$scope', '$filter'];
+function MsgController($scope, $filter) {
+  $scope.lunchmenufield = "";
+  $scope.lunchmessage = "";
+
+  $scope.sayMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    var output = $filter('uppercase')(msg);
+    return output;
+  };
+
+  $scope.feedYaakov = function () {
+    $scope.stateOfBeing = "fed";
+  };
+  
+  $scope.checkLunch = function() {
+    console.log("entered checkLunch function");  
+    console.log($scope.lunchmenufield);
+    
+    var lunchmenuitemsarray = $scope.lunchmenufield.split(',');
+    if ($scope.lunchmenufield === "" ) {
+    console.log("Item list is empty, please enter list of meals");
+    $scope.lunchmessage = "Please enter data first";
+    } else if (lunchmenuitemsarray.length > 3) {
+    console.log("That's too much, you have more than 3 meals!!");                
+    $scope.lunchmessage = "Too much!";
+    } else {
+    console.log("You have " + lunchmenuitemsarray.length + " items!!");                        
+    console.log("First item is" + lunchmenuitemsarray[0] + "!!");                        
+    $scope.lunchmessage = "Enjoy!";
+    };
+  };
+}
+
+})();
